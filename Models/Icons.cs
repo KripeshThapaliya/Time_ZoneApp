199c0b07c9d170c47f827e431fc2b6f7a9e1cd3e
a00270681@mycambrian.ca <A00270681@mycambrian.ca>
2024-03-21 13:20:53 -0400

    Updated the icons for the app

--- Models/Icons.cs
+++ Models/Icons.cs
@@ -1,1 +1,10 @@
 ﻿namespace TimeZoneApp.Models;
+
+public static class Icons
+{
+    public const string Home = "home.png";
+    public const string ContactUs = "contact_us.png";
+    public const string About = "about.png";
+    public const string Menu = "dashboard_1.png";
+    public const string GoBack = "back_arrow.png";
+}
