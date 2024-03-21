827653d7b8461f61c862d7c5a0b27d789fcf4ed4
a00270681@mycambrian.ca <A00270681@mycambrian.ca>
2024-03-21 13:31:52 -0400

    Creating function for login

--- ViewModels/LoginPageViewModel.cs
+++ ViewModels/LoginPageViewModel.cs
@@ -9,4 +9,45 @@ namespace TimeZoneApp.ViewModels;
 
 public partial class LoginPageViewModel(IAccountService accountService) : BaseViewModel
 {
+     [ObservableProperty]
+    private string _userName = string.Empty;
+
+    [ObservableProperty]
+    private string _password = string.Empty;
+
+    [RelayCommand]
+    public async Task Login()
+    {
+        if (!string.IsNullOrWhiteSpace(UserName) && !string.IsNullOrWhiteSpace(Password))
+        {
+            var userInfo = accountService.Login(UserName, Password);
+
+            if (userInfo != null)
+            {
+                if (Preferences.ContainsKey(nameof(App.UserInfo)))
+                {
+                    Preferences.Remove(nameof(App.UserInfo));
+                }
+
+                var userDetails = JsonConvert.SerializeObject(userInfo);
+                Preferences.Set(nameof(App.UserInfo), userDetails);
+                App.UserInfo = userInfo;
+
+                Shell.Current.FlyoutHeader = new FlyoutHeaderControl();
+                ResetInputs();
+
+                await Shell.Current.GoToAsync($"//{nameof(HomePage)}");
+            }
+            else
+            {
+                await Shell.Current.DisplayAlert("Error", "Invalid Credentials", "OKAY");
+            }
+        }
+        else
+        {
+            await Shell.Current.DisplayAlert("Error", "All fields are required", "OKAY");
+        }
+    }
+
+    
 }
