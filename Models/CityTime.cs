94867d0507960daa6568831038e69eb1f0d9ab05
a00270681@mycambrian.ca <A00270681@mycambrian.ca>
2024-03-21 13:17:22 -0400

    Created Constructor for city details

--- Models/CityTime.cs
+++ Models/CityTime.cs
@@ -11,3 +11,26 @@ public class Location
     public string Name { get; set; } = default!;
     public DateTime LocalTime { get; set; }
 }
+
+public class Current
+{
+    public float Temp_C { get; set; }
+    public float Temp_F { get; set; }
+    public Condition Condition { get; set; } = new();
+}
+
+public class Condition
+{
+    public string Text { get; set; } = default!;
+    public string Icon { get; set; } = default!;
+}
+
+public class CityDetail
+{
+    public string Name { get; set; } = default!;
+    public string TempCondition { get; set; } = default!;
+    public string Icon { get; set; } = default!;
+    public string Time { get; set; } = default!;
+    public string Date { get; set; } = default!;
+
+}
