de710cdb240e7f29d8b99328c836f4fbdd90853d
a00270681@mycambrian.ca <A00270681@mycambrian.ca>
2024-03-21 13:14:48 -0400

    Made a Constroctor for CityTime and Location

--- Models/CityTime.cs
+++ Models/CityTime.cs
@@ -1,1 +1,13 @@
 ﻿namespace TimeZoneApp.Models;
+
+public class CityTime
+{
+    public Location Location { get; set; } = new();
+    public Current Current { get; set; } = new();
+}
+
+public class Location
+{
+    public string Name { get; set; } = default!;
+    public DateTime LocalTime { get; set; }
+}
