eaa747ff6c6a610425487b4d7124490f641ad51a
Roshan Karki <A00276018@mycambrian.ca>
2024-03-21 14:11:25 -0400

    created default user account.

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -4,5 +4,19 @@ namespace TimeZoneApp.Services;
 
 public class AccountService : IAccountService
 {
+  private readonly List<UserInfo> _users = [];
+
+    public AccountService()
+    {
+        if (_users.Count != 0) return;
+        var user = new UserInfo()
+        {
+            Id = 1,
+            UserName = "admin",
+            Email = "admin@timezoneapp.com",
+            Password = "12345"
+        };
+        _users.Add(user);
+    }
 
 }
