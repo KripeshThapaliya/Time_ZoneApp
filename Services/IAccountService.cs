33e60bc7f362a2790f693c10adb1a8e972f92287
a00270681@mycambrian.ca <A00270681@mycambrian.ca>
2024-03-25 12:11:26 -0400

    Created a service for login and sign up

--- Services/IAccountService.cs
+++ Services/IAccountService.cs
@@ -4,5 +4,10 @@ namespace TimeZoneApp.Services;
 
 public interface IAccountService
 {
+    UserInfo? Login(string username, string password);
+
+    UserInfo? SignUp(string username, string email, string password);
+
+    bool UserWithSameUserNameAlreadyExists(string username, string email);
    
 }
