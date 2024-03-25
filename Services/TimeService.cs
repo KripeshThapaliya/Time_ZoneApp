7ca0048074b6db6349de75f22c58159192a69ce2
a00270681@mycambrian.ca <A00270681@mycambrian.ca>
2024-03-25 12:29:31 -0400

    Created TimeService class for searching cities and retrieving their respective times using the WeatherAPI.

--- Services/TimeService.cs
+++ Services/TimeService.cs
@@ -5,5 +5,50 @@ namespace TimeZoneApp.Services;
 
 public class TimeService : ITimeService
 {
+     private const string ApiKey = "7e507d9dcb934999be4165417241503";
+    private const string SearchBaseAddress = "https://api.weatherapi.com/v1/search.json?q=";
+    private const string GetTimeBaseAddress = "https://api.weatherapi.com/v1/current.json?q=";
+
+    public IReadOnlyCollection<City> SearchCities(string searchText)
+    {
+        try
+        {
+            var client = new HttpClient();
+
+            var url = $"{SearchBaseAddress}{searchText}&key={ApiKey}";
+
+            var response = client.GetStringAsync(url).Result;
+            var result = JsonConvert.DeserializeObject<List<City>>(response);
+            if (result != null) return result;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return new City[] { };
+    }
+
+    public CityTime GetTime(string city)
+    {
+        try
+        {
+            var client = new HttpClient();
+
+            var url = $"{GetTimeBaseAddress}{city}&key={ApiKey}";
+
+            var response = client.GetStringAsync(url).Result;
+            var result = JsonConvert.DeserializeObject<CityTime>(response);
+            if (result != null) return result;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+
+        return new CityTime();
+    }
    
 }
