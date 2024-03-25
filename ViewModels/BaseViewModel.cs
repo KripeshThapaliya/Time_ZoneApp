39e3e977b595fe5176460d6b903f5a2860514b50
a00270681@mycambrian.ca <A00270681@mycambrian.ca>
2024-03-25 12:49:31 -0400

    Implemented observable properties for _isBusy and _title.

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -4,5 +4,10 @@ namespace TimeZoneApp.ViewModels;
 
 public partial class BaseViewModel : ObservableObject
 {
+     [ObservableProperty]
+    public bool _isBusy;
+
+    [ObservableProperty]
+    public string _title;
    
 }
