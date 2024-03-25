2eb8d5658f05209eea777fcbd757c527fb20f54d
a00270681@mycambrian.ca <A00270681@mycambrian.ca>
2024-03-25 12:37:09 -0400

    Added sign-out functionality

--- ViewModels/AppShellViewModel.cs
+++ ViewModels/AppShellViewModel.cs
@@ -3,5 +3,15 @@
 namespace TimeZoneApp.ViewModels;
 public partial class AppShellViewModel : BaseViewModel
 {
+    [RelayCommand]
+    async void SignOut()
+    {
+        if (Preferences.ContainsKey(nameof(App.UserInfo)))
+        {
+            Preferences.Remove(nameof(App.UserInfo));
+        }
+
+        await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+    }
    
 }
