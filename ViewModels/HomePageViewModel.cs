a6856f6a0811adda01c2c1cdedcde74e23130d4e
a00270681@mycambrian.ca <A00270681@mycambrian.ca>
2024-03-25 13:16:51 -0400

    Implemented observable properties and methods for managing home and destination city search and details

--- ViewModels/HomePageViewModel.cs
+++ ViewModels/HomePageViewModel.cs
@@ -7,5 +7,39 @@ namespace TimeZoneApp.ViewModels;
 
 public partial class HomePageViewModel : BaseViewModel
 {
+    
+    [ObservableProperty]
+    private IReadOnlyCollection<City> _homeCitiesSearch = [];
+
+    [ObservableProperty]
+    private City _homeCitySelectedKey = new();
+
+    [ObservableProperty]
+    private string _homeCitySearchText = string.Empty;
+
+    [ObservableProperty]
+    private bool _showHomeCityDetail = false;
+
+    [ObservableProperty]
+    private CityDetail _homeCityDetail = new();
+
+
+    [ObservableProperty]
+    private IReadOnlyCollection<City> _destinationCitiesSearch = [];
+
+    [ObservableProperty]
+    private City _destinationCitySelectedKey = new();
+
+    [ObservableProperty]
+    private string _destinationCitySearchText = string.Empty;
+
+    [ObservableProperty]
+    private bool _showDestinationCityDetail = false;
+
+    [ObservableProperty]
+    private CityDetail _destinationCityDetail = new();
+
+    private readonly ITimeService _timeService;
+
 
 }
